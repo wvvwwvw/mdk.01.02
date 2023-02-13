044613e0bfce3aa0ec0d5d6e16a67947a1fd094c
виолетта <115283452+wvvwwvw@users.noreply.github.com>
2023-02-13 11:13:18 +0300

    Add files via upload

---
+++ Marathon Skills 2016/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marathon_Skills_2016
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label7.Visible = true;
+            label7.Text = "МЦК Лужники";
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            pictureBox6.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label7.Visible = true;
+            label7.Text = "Новодевичий монастырь";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            pictureBox6.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label7.Visible = true;
+            label7.Text = "Метро Воробьевы горы";
+            pictureBox5.Location = new Point(660, 428);
+            label4.Location = new Point(821, 455);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            pictureBox6.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label7.Visible = true;
+            label7.Text = "Стадион Лужники";
+            pictureBox5.Location = new Point(660, 428);
+            label4.Location = new Point(821, 455);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = true;
+            label7.Visible = true;
+            label7.Text = "Метро Киевская";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = true;
+            label7.Visible = true;
+            label7.Text = "Здание РАН";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = true;
+            pictureBox5.Location = new Point(660, 343);
+            pictureBox6.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = false;
+            label4.Visible = true;
+            label4.Location = new Point(810, 372);
+            label5.Visible = true;
+            label7.Visible = true;
+            label7.Text = "МИД";
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = false;
+            label5.Visible = true;
+            label7.Visible = true;
+            label7.Text = "Парк Горького";
+
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
